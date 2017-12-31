789fba3ed90517cedf62d6c8cbba830e938ef962
giang <1337vn@gmail.com>
2017-12-31 14:51:12 +0000

    Understand about JSX-babel, refs vs ref, default props, how render work

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,36 @@
+var GreetingComponent = React.createClass({
+  getDefaultProps: function(){
+    return {
+      name: 'React',
+      message: 'You rock',
+    }
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    var name 
+    alert(`Hello ${this.refs.name.value}`)
+  },
+  render: function(){
+    var name = this.props.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1>My first React and Express server</h1>
+        <p>This is so awesome</p>
+        <em>{name}</em>
+        <q> {message + '!!'}</q>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' ref='name'></input>
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
+});
+var fullName = 'Chau Giang';
 ReactDOM.render(
-  <h1>Hello React</h1>,
+  <GreetingComponent
+    name={fullName}
+  />,
   document.getElementById('app')
 )
